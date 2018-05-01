6d148e84854332dbdd98dc1d76c2a501071b773d
fyao638 <fyao638@PR14.hhs.win.fuhsd.org>
2018-05-01 11:36:05 -0700

    plz no

--- Animation Demo/src/Mario.java
+++ Animation Demo/src/Mario.java
@@ -9,9 +9,12 @@ public class Mario extends Sprite {
 
 	public static final int MARIO_WIDTH = 40;
 	public static final int MARIO_HEIGHT = 60;
+	
+	private double yVel;
 
 	public Mario(PImage img, int x, int y) {
 		super(img, x, y, MARIO_WIDTH, MARIO_HEIGHT);
+		yVel = 0;
 	}
 
 	// METHODS
@@ -24,7 +27,19 @@ public class Mario extends Sprite {
 	}
 
 	public void act(ArrayList<Shape> obstacles) {
+		
+		yVel += 0.05;
+		
 		// FALL (and stop when a platform is hit)
+		for(Shape s : obstacles) {
+			if(s.intersects(this.getX(),this.getY() , MARIO_WIDTH, MARIO_HEIGHT)) {
+				yVel = 0;
+			}
+		}
+		
+		
+		this.moveByAmount( 0 , yVel);
+		
 	}
 
 
