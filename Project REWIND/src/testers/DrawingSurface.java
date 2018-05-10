3b949a65f896ae4a3769e04f1b71b4f75ab9389b
frankYao <frankYao@192.168.20.23>
2018-05-09 18:40:14 -0700

    MOVEMENT BROKEN, MAJOR DRAWINGSURFACE CHANGES

--- Project REWIND/src/testers/DrawingSurface.java
+++ Project REWIND/src/testers/DrawingSurface.java
@@ -1,13 +1,9 @@
 package testers;
 
-import java.awt.event.KeyEvent;
-import java.awt.event.MouseEvent;
-import java.awt.geom.Point2D;
 import java.util.ArrayList;
+
 import processing.core.PApplet;
-import processing.core.PImage;
-import sprites.Bullet;
-import sprites.Player;
+
 
 public class DrawingSurface extends PApplet {
 /* Ghost class ?
@@ -18,209 +14,39 @@ public class DrawingSurface extends PApplet {
  * 
  * 
  */
-	public static final int DRAWING_WIDTH = 800;
-	public static final int DRAWING_HEIGHT = 600;
-
-	private Player p1, p1Ghost;
-
+	private PlayScreen playScreen;
 	private ArrayList<Integer> keys;
-	
-	private ArrayList<Bullet> bullets;
-	
-	private ArrayList<PImage> assets;
-	
-	private ArrayList<Point2D.Double> prevLocs;
-	private ArrayList<Point2D.Double> prevMouseLocs;
-	
-	private Map map;
-	private Hud hud;
-
-	private long shotReadyTime, rewindReadyTime, secondaryReadyTime, shiftReadyTime, ghostReappearTime;
-	
-	private float abilWidth, abilHeight;
 	
 	public DrawingSurface() {
 		super();
-		assets = new ArrayList<PImage>();
+		playScreen = new PlayScreen();
 		keys = new ArrayList<Integer>();
-		bullets = new ArrayList<Bullet>();
 		
-		hud = new Hud();
-		prevLocs = new ArrayList<Point2D.Double>();
-		shotReadyTime = 0;
-		rewindReadyTime = 0;
-		secondaryReadyTime = 0;
-		shiftReadyTime = 0;
-		ghostReappearTime = 0;
-		prevMouseLocs = new ArrayList<Point2D.Double>();
-		abilWidth = 100;
-		abilHeight = 100;
 	}
 
-
-	public void spawnNewPlayer() {
-		p1 = new Player(assets.get(0), DRAWING_WIDTH/2-Player.PLAYER_WIDTH/2,50);
-	}
-	
-	public void spawnNewGhost() {
-		p1Ghost = new Player(assets.get(1), (int)prevLocs.get(0).getX(), (int)prevLocs.get(0).getY());
-	}
-	
-	public void runMe() {
-		runSketch();
-	}
-
-	// The statements in the setup() function 
-	// execute once when the program begins
 	public void setup() {
-		noStroke();
-		assets.add(loadImage("player.png"));
-		assets.add(loadImage("ghost.png"));
-		assets.add(loadImage("bullet.png"));
-		assets.add(loadImage("star.png"));
-		assets.add(loadImage("crosshair.png"));
-		assets.add(loadImage("time.png"));
-		assets.add(loadImage("starIcon.png"));
-		assets.add(loadImage("flash.png"));
-		assets.add(loadImage("wall.png"));
-		assets.add(loadImage("wall2.png"));
-		
-		map = new Map(assets.get(8), assets.get(9));
-		spawnNewPlayer();
-		
-		Point2D.Double p = new Point2D.Double(p1.getX(), p1.getY());
-		prevLocs.add(p);
-		
-		spawnNewGhost();
+		playScreen.setup(this);
 	}
 
 	// The statements in draw() are executed until the 
 	// program is stopped. Each statement is executed in 
 	// sequence and after the last line is read, the first 
 	// line is executed again.
-	int timer = 0;
 	public void draw() {
-
-		Point2D.Double p = new Point2D.Double(p1.getX(), p1.getY());
-		prevLocs.add(p);
-		if (prevLocs.size() > 120)
-			prevLocs.remove(0);
-		
-
-		Point2D.Double pMouse = new Point2D.Double(mouseX, mouseY);
-		prevMouseLocs.add(pMouse);
-		if (prevMouseLocs.size() > 120)
-			prevMouseLocs.remove(0);
-		
-		p1Ghost.setX((int)prevLocs.get(0).getX());
-		p1Ghost.setY((int)prevLocs.get(0).getY());
-		
-		background(128,128,128);  
-
-		float ratioX = (float)width/DRAWING_WIDTH;
-		float ratioY = (float)height/DRAWING_HEIGHT;
-
-		scale(ratioX, ratioY);
-		
-		p1.turnToward(mouseX / ratioX, mouseY / ratioY);
-		p1Ghost.turnToward((float)prevMouseLocs.get(0).getX() / ratioX, (float)prevMouseLocs.get(0).getY() / ratioY);
-
-		if (isPressed(KeyEvent.VK_A))
-			p1.walk(-1, 0, map.getObstacles());
-		if (isPressed(KeyEvent.VK_D))
-			p1.walk(1, 0, map.getObstacles());
-		if (isPressed(KeyEvent.VK_W))
-			p1.walk(0, -1, map.getObstacles());
-		if (isPressed(KeyEvent.VK_S))
-			p1.walk(0, 1, map.getObstacles());
-		if (isPressed(KeyEvent.VK_R)) {
-			if(rewindReadyTime - millis() <= 0) {
-				p1.moveToLocation(prevLocs.get(0).getX(), prevLocs.get(0).getY());
-				
-				rewindReadyTime = millis() + 15000;
-				ghostReappearTime = millis() + 2000;
-			}
-		}
-		if (isPressed(KeyEvent.VK_SHIFT)) {
-			if(shiftReadyTime - millis() <= 0) {
-				
-				p1.shiftAbility(map.getObstacles());
-				
-				shiftReadyTime = millis() + 7000;
-				
-			}
-		}
-			
-		//TESTING HEALTH
-		if(isPressed(KeyEvent.VK_SPACE)) { 
-			p1.changeHealth(-1);
-			System.out.println(p1.getHealth());
-		}
-
-		// Draw abilities
-		
-		if(mousePressed) {
-			if(mouseButton == LEFT) {
-				if(shotReadyTime - millis() <= 0) {
-					bullets.add(p1.shoot(assets.get(2)));
-					shotReadyTime = millis() + 1000;
-				}
-			}
-			else if(mouseButton == RIGHT) {
-				if(secondaryReadyTime - millis() <= 0) {
-					if(p1.getType() == 1) {
-						ArrayList<Bullet> fan = p1.secondaryShoot(assets.get(3));
-						for(Bullet b : fan) {
-							bullets.add(b);
-						}
-						secondaryReadyTime = millis() + 7000;
-					}
-				}
-			}
-			else if(mouseButton == CENTER) {
-				if(secondaryReadyTime - millis() <= 0) {
-					bullets.add(p1.secondaryShoot2(assets.get(2)));
-					secondaryReadyTime = millis() + 1000;
-				}
-			}
-		}
-
-		if (bullets.size() > 0) {
-			for(int i = 0; i < bullets.size(); i++) {
-					bullets.get(i).act();
-					bullets.get(i).draw(this);
-					if (bullets.get(i).checkObstacles(map.getObstacles())) {
-						bullets.remove(i);
-					}
-			}
-		}
-
-		fill(100);
-		map.draw(this);
-		
-		// draw the players after the bullets so the bullets don't appear above the gun
-		if(ghostReappearTime - millis() < 0) {
-			p1Ghost.draw(this);
-		}
-		p1.draw(this);
-
-		hud.draw(this, p1.getHealth(), assets.get(4), assets.get(5), assets.get(6),assets.get(7), shotReadyTime, rewindReadyTime, secondaryReadyTime, shiftReadyTime, millis(), abilWidth, abilHeight);
-		
-		timer++;
+		playScreen.draw(this);
+	}
+	
+	public void keyPressed(PApplet a) {
+		keys.add(a.keyCode);
 	}
 
-	public void keyPressed() {
-		keys.add(keyCode);
-	}
-
-	public void keyReleased() {
-		while(keys.contains(keyCode))
-			keys.remove(new Integer(keyCode));
+	public void keyReleased(PApplet a ) {
+		while(keys.contains(a.keyCode))
+			keys.remove(new Integer(a.keyCode));
 	}
 
 	public boolean isPressed(Integer code) {
 		return keys.contains(code);
 	}
-	
 }
 
--- Project REWIND/src/testers/PlayScreen.java
+++ Project REWIND/src/testers/PlayScreen.java
@@ -1,8 +1,190 @@
 package testers;
 
+import java.awt.event.KeyEvent;
+import java.awt.geom.Point2D;
+import java.util.ArrayList;
 import processing.core.PApplet;
+import processing.core.PConstants;
+import processing.core.PImage;
+import sprites.Bullet;
+import sprites.Player;
 
-public class PlayScreen extends PApplet {
+public class PlayScreen{
 	
+	public static final int DRAWING_WIDTH = 800;
+	public static final int DRAWING_HEIGHT = 600;
+
+	private Player p1, p1Ghost;
+	
+	private ArrayList<Bullet> bullets;
+	
+	private ArrayList<PImage> assets;
+	
+	private ArrayList<Point2D.Double> prevLocs;
+	private ArrayList<Point2D.Double> prevMouseLocs;
+	
+	private Map map;
+	private Hud hud;
+
+	private long shotReadyTime, rewindReadyTime, secondaryReadyTime, shiftReadyTime, ghostReappearTime;
+	
+	private float abilWidth, abilHeight;
+	
+	
+	public PlayScreen() {
+		super();
+		assets = new ArrayList<PImage>();
+		bullets = new ArrayList<Bullet>();
+		hud = new Hud();
+		prevLocs = new ArrayList<Point2D.Double>();
+		shotReadyTime = 0;
+		rewindReadyTime = 0;
+		secondaryReadyTime = 0;
+		shiftReadyTime = 0;
+		ghostReappearTime = 0;
+		prevMouseLocs = new ArrayList<Point2D.Double>();
+		abilWidth = 100;
+		abilHeight = 100;
+	}
+	public void spawnNewPlayer() {
+		p1 = new Player(assets.get(0), DRAWING_WIDTH/2-Player.PLAYER_WIDTH/2,50);
+	}
+	
+	public void spawnNewGhost() {
+		p1Ghost = new Player(assets.get(1), (int)prevLocs.get(0).getX(), (int)prevLocs.get(0).getY());
+	}
+	
+	public void setup(DrawingSurface drawer) {
+		drawer.noStroke();
+		assets.add(drawer.loadImage("player.png"));
+		assets.add(drawer.loadImage("ghost.png"));
+		assets.add(drawer.loadImage("bullet.png"));
+		assets.add(drawer.loadImage("star.png"));
+		assets.add(drawer.loadImage("crosshair.png"));
+		assets.add(drawer.loadImage("time.png"));
+		assets.add(drawer.loadImage("starIcon.png"));
+		assets.add(drawer.loadImage("flash.png"));
+		assets.add(drawer.loadImage("wall.png"));
+		assets.add(drawer.loadImage("wall2.png"));
+		
+		map = new Map(assets.get(8), assets.get(9));
+		spawnNewPlayer();
+		
+		Point2D.Double p = new Point2D.Double(p1.getX(), p1.getY());
+		prevLocs.add(p);
+		
+		spawnNewGhost();
+	}
+	int timer = 0;
+	public void draw(DrawingSurface drawer) {
+
+		Point2D.Double p = new Point2D.Double(p1.getX(), p1.getY());
+		prevLocs.add(p);
+		if (prevLocs.size() > 120)
+			prevLocs.remove(0);
+		
+
+		Point2D.Double pMouse = new Point2D.Double(drawer.mouseX, drawer.mouseY);
+		prevMouseLocs.add(pMouse);
+		if (prevMouseLocs.size() > 120)
+			prevMouseLocs.remove(0);
+		
+		p1Ghost.setX((int)prevLocs.get(0).getX());
+		p1Ghost.setY((int)prevLocs.get(0).getY());
+		
+		drawer.background(128,128,128);  
+
+		float ratioX = (float)drawer.width/DRAWING_WIDTH;
+		float ratioY = (float)drawer.height/DRAWING_HEIGHT;
+
+		drawer.scale(ratioX, ratioY);
+		
+		p1.turnToward(drawer.mouseX / ratioX, drawer.mouseY / ratioY);
+		p1Ghost.turnToward((float)prevMouseLocs.get(0).getX() / ratioX, (float)prevMouseLocs.get(0).getY() / ratioY);
+
+		if(drawer.isPressed(KeyEvent.VK_A))
+			p1.walk(-1, 0, map.getObstacles());
+		if (drawer.isPressed(KeyEvent.VK_D))
+			p1.walk(1, 0, map.getObstacles());
+		if (drawer.isPressed(KeyEvent.VK_W))
+			p1.walk(0, -1, map.getObstacles());
+		if (drawer.isPressed(KeyEvent.VK_S))
+			p1.walk(0, 1, map.getObstacles());
+		if (drawer.isPressed(KeyEvent.VK_R)) {
+			if(rewindReadyTime -drawer.millis() <= 0) {
+				p1.moveToLocation(prevLocs.get(0).getX(), prevLocs.get(0).getY());
+				
+				rewindReadyTime = drawer.millis() + 15000;
+				ghostReappearTime = drawer.millis() + 2000;
+			}
+		}
+		if (drawer.isPressed(KeyEvent.VK_SHIFT)) {
+			if(shiftReadyTime - drawer.millis() <= 0) {
+				
+				p1.shiftAbility(map.getObstacles());
+				
+				shiftReadyTime = drawer.millis() + 7000;
+				
+			}
+		}
+			
+		//TESTING HEALTH
+		if(drawer.isPressed(KeyEvent.VK_SPACE)) { 
+			p1.changeHealth(-1);
+			System.out.println(p1.getHealth());
+		}
+
+		// Draw abilities
+		
+		if(drawer.mousePressed) {
+			if(drawer.mouseButton == PConstants.LEFT) {
+				if(shotReadyTime - drawer.millis() <= 0) {
+					bullets.add(p1.shoot(assets.get(2)));
+					shotReadyTime = drawer.millis() + 1000;
+				}
+			}
+			else if(drawer.mouseButton == PConstants.RIGHT) {
+				if(secondaryReadyTime - drawer.millis() <= 0) {
+					if(p1.getType() == 1) {
+						ArrayList<Bullet> fan = p1.secondaryShoot(assets.get(3));
+						for(Bullet b : fan) {
+							bullets.add(b);
+						}
+						secondaryReadyTime = drawer.millis() + 7000;
+					}
+				}
+			}
+			else if(drawer.mouseButton == PConstants.CENTER) {
+				if(secondaryReadyTime - drawer.millis() <= 0) {
+					bullets.add(p1.secondaryShoot2(assets.get(2)));
+					secondaryReadyTime = drawer.millis() + 1000;
+				}
+			}
+		}
+
+		if (bullets.size() > 0) {
+			for(int i = 0; i < bullets.size(); i++) {
+					bullets.get(i).act();
+					bullets.get(i).draw(drawer);
+					if (bullets.get(i).checkObstacles(map.getObstacles())) {
+						bullets.remove(i);
+					}
+			}
+		}
+
+		drawer.fill(100);
+		map.draw(drawer);
+		
+		// draw the players after the bullets so the bullets don't appear above the gun
+		if(ghostReappearTime - drawer.millis() < 0) {
+			p1Ghost.draw(drawer);
+		}
+		p1.draw(drawer);
+
+		hud.draw(drawer, p1.getHealth(), assets.get(4), assets.get(5), assets.get(6),assets.get(7), shotReadyTime, rewindReadyTime, secondaryReadyTime, shiftReadyTime, drawer.millis(), abilWidth, abilHeight);
+		
+		timer++;
+	
+	}
 }
 
